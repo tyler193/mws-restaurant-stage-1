ca0022343bf0b7792050edf59b5d7c516bdb6d1a
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-09-05 21:36:23 -0500

    Added a11y features for better navigation

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'Restaurant picture';
   li.append(image);
 
   const name = document.createElement('h1');
@@ -178,6 +179,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex = "4";
   li.append(more)
 
   return li
