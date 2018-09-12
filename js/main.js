0133ae51aefc5622cdfae54318a5fc494051f7e4
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-09-12 18:10:22 -0500

    Added better alt text to images

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = 'Picture of ';
+  image.alt = restaurant.name + ' atmosphere';
   li.append(image);
 
   const name = document.createElement('h2');
