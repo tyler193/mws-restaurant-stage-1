582d27ed27b10df98cb24fab89265722d493dfeb
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-09-12 14:09:17 -0500

    Updated responsiveness and sw2 file

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = 'Picture of ' + name;
+  image.alt = 'Picture of ';
   li.append(image);
 
   const name = document.createElement('h1');
